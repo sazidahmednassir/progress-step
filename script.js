b5af616293f844696868f1689b62d5e93628c750
sazidahmednassir <sazidahmednassir@gmail.com>
2022-03-12 23:49:40 -0800

    eight  commit

--- script.js
+++ script.js
@@ -7,16 +7,46 @@ let currentActive=1;
 
 next.addEventListener('click',  ()=>{
     currentActive++;
+    // console.log(currentActive)
     if(currentActive > circles.length){
         currentActive=circles.length;
 
     }
+    update()
 })
 
 prev.addEventListener('click',  ()=>{
     currentActive--;
+    // console.log(currentActive)
     if(currentActive < 1){
         currentActive=1;
 
     }
+    update()
 })
+
+
+function update(){
+   circles.forEach((circle, idx)=>{
+       
+           if(idx<currentActive){
+            // console.log(idx)
+            // console.log(currentActive)
+               circle.classList.add('active')
+           }else{
+               circle.classList.remove('active')
+           }
+   })
+   const actives=document.querySelectorAll('.active')
+   progress.style.width=((actives.length-1)/ (circles.length-1)) *100 + '%';
+
+   if(currentActive===1){
+         prev.disabled=true;
+
+   }else if(currentActive == circles.length){
+       next.disabled=true;
+   }else{
+    next.disabled=false;
+    prev.disabled=false;
+   }
+}
